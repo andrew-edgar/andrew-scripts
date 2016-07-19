ff7c87bd10eab49e1e2efdd6f7d5dbc008f72ba9
Andrew Edgar <aedgar@ca.ibm.com>
2016-07-19 14:47:24 -0600

    Add stemcell download to script

--- releases/refreshReleases.sh
+++ releases/refreshReleases.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+aria2c -x 15 -j 15 --auto-file-renaming=false -d . https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
 aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry/cflinuxfs2-rootfs-release
 aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry/cf-mysql-release
 aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release
