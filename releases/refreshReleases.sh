2152ee80998cbe4ee33d2d5d1c5e69dbd1a07630
Andrew Edgar <aedgar1@de.ibm.com>
2017-03-01 10:42:22 +0100

    Updated to remove old releases we do not need

--- releases/refreshReleases.sh
+++ releases/refreshReleases.sh
@@ -4,7 +4,6 @@ aria2c -x 15 -j 15 --auto-file-renaming=false -d . https://bosh.io/d/stemcells/b
 aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry/cflinuxfs2-rootfs-release
 aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry/cf-mysql-release
 aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release
-aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release
-aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry-incubator/garden-runc-release
+aria2c -x 15 -j 15 --auto-file-renaming=false -d . http://bosh.io/d/github.com/cloudfoundry/garden-runc-release
 
 
